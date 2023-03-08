214bbfa0ac94b631b85091b62b6067b56822ea57
menspurlos <dima@grishkin.net>
2023-03-08 12:00:00 +0700

    Initial commit

---
+++ APIproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIproject
+//
+//  Created by macBook on 08.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
