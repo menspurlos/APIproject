3b61e9d93faef1006a8fc79909ca8d1ba48ed92e
menspurlos <dima@grishkin.net>
2023-04-01 10:35:01 +0700

    поменял на UIVC

--- APIproject/DataDisplay/View/ViewController.swift
+++ APIproject/DataDisplay/View/ViewController.swift
@@ -15,43 +15,53 @@ protocol ViewInputProtocol: AnyObject {
 
     //тут ветка без тэйбла
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
     
     var docs: [DataForDisplay] = []
     var presenter: ViewOutPutProtocol!
     private let configurator: ConfiguratorInputProtocol = Configurator()
-        
+    
+//    private lazy var tableView: UITableView = {
+//        let table = UITableView(frame: .zero)
+//        return table
+//    }()
+    var tableView: UITableView
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
         navigationControllerConfigure()
- 
+        
         configurator.configure(with: self)
         presenter.prepareDataForDisplay()
         
     }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return docs.count
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
-
+        
         cell.set(object: docs[indexPath.row])
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
     
 }
+
 
 extension ViewController: ViewInputProtocol {
     
